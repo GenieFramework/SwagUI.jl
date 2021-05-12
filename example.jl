32b23d2b84759c40d3dc691d6521c52570ceab29
jiachengzhang1 <jiachengzhang1@email.arizona.edu>
2021-05-11 22:00:12 -0700

    add example

--- example.jl
+++ example.jl
@@ -1,13 +1,9 @@
 using Genie, Genie.Router
 using Genie.Renderer, Genie.Renderer.Html, Genie.Renderer.Json
+using JSON
+using SwaggerUI
 
 swagger_document = JSON.parsefile("./swagger.json")
-swagger_options = Dict{String, Any}()
-swagger_options["validatorUrl"] = nothing
-# swagger_options["url"] = "https://petstore.swagger.io/v2/swagger.json"
-# options = Options(show_explorer=false, swagger_options=swagger_options)
-
-# options.custom_css = ".swagger-ui .topbar { display: none }"
 
 urls = Array{Dict{String, Any}, 1}()
 url1 = Dict{String, Any}()
@@ -19,13 +15,19 @@ url2["name"] = "Spec2"
 push!(urls, url1)
 push!(urls, url2)
 
-swagger_options["urls"] = urls
 
-options = Options(swagger_options=swagger_options)
+options = Options()
+# options.custom_css = ".swagger-ui .topbar { display: none }"
+options.show_explorer = false
 
-route("/") do 
-    render_swagger(nothing, options=options)
-    # render_swagger(swagger_document, options=options)
+# options.swagger_options["validatorUrl"] = nothing
+# options.swagger_options["url"] = "https://petstore.swagger.io/v2/swagger.json"
+# options.swagger_options["urls"] = urls
+
+
+route("/docs") do 
+    # render_swagger(nothing, options=options)
+    render_swagger(swagger_document, options=options)
 end
 
 up(8001, async = false)
